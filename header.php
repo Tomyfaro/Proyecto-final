105881995d4f2504d9de43bcaf8dc22129e447ba
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-30 16:42:21 -0500

    m

---
+++ header.php
@@ -0,0 +1,40 @@
+<style>
+header{
+    text-align: right;
+}
+nav {
+	float: right;
+}
+nav ul {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	padding-right: 20px;
+}
+	
+nav ul li a {
+	display: inline-block;
+    line-height: 80px;
+    color: white;
+	}
+			
+nav ul li a {
+	display: block;
+	padding: 0 10px;
+	text-decoration: none;
+	}
+			
+nav ul li a:hover {
+	background: black;
+	}
+</style>
+<header>
+        <nav>
+            <ul>
+            <li><a href="informacion.php">¿Qué es DUP?</a> </li>
+            <li><a href="adoptar_mascota.php">Adopta</a> </li>
+            <li><a href="esterilizacion.php">Esterlización de Mascotas</a> </li>
+            <li><a href="animalperdido.php">Reporta un animal perdido o abandonado</a></li>
+            </ul>
+        </nav>
+</header>
